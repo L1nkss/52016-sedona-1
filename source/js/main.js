a173a677451b2f90937fac7cef3ee0a98437cba0
L1nks <l1nks@inbox.ru>
2018-10-20 00:47:45 +0500

    правки дизайна

---
+++ source/js/main.js
@@ -0,0 +1,21 @@
+var menu = document.querySelector('.main-menu');
+var button = document.querySelector('.main-menu__button-toggle');
+var form_button = document.querySelector('.user-opinion__button');
+
+var user_name = document.querySelector('[name=user-name]')
+var user_surname = document.querySelector('[name=user-surname]')
+var user_phone = document.querySelector('[name=user-mobile]')
+var user_email = document.querySelector('[name=user-email]')
+
+function close_menu() {
+  menu.classList.add('hide');
+  button.classList.remove('main-menu__button-toggle--toggle-modif');
+}
+
+function open_menu() {
+  menu.classList.toggle('hide');
+  button.classList.toggle('main-menu__button-toggle--toggle-modif');
+}
+
+window.onload = close_menu();
+button.addEventListener("click",open_menu);
