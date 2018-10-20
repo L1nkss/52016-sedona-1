e295a81e5423a2425aaf234e772fc03ad3d37b43
L1nks <l1nks@inbox.ru>
2018-10-21 03:39:11 +0500

    правки по замечаниям

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,5 @@
 var menu = document.querySelector('.main-menu');
-var button = document.querySelector('.main-menu__button-toggle');
+var button = document.querySelector('.menu-action__button-toggle');
 var form_button = document.querySelector('.user-opinion__button');
 
 var user_name = document.querySelector('[name=user-name]')
@@ -9,12 +9,12 @@ var user_email = document.querySelector('[name=user-email]')
 
 function close_menu() {
   menu.classList.add('hide');
-  button.classList.remove('main-menu__button-toggle--toggle-modif');
+  button.classList.remove('menu-action__button-toggle--toggle-modif');
 }
 
 function open_menu() {
   menu.classList.toggle('hide');
-  button.classList.toggle('main-menu__button-toggle--toggle-modif');
+  button.classList.toggle('menu-action__button-toggle--toggle-modif');
 }
 
 window.onload = close_menu();
